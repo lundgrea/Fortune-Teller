e1b3b1ff334fdbb08b07d8b23f659c96295a55d1
Alyssa Lundgren <lundgrea@gmail.com>
2019-11-13 08:00:12 -0700

    Add gender and advice api calls

--- apiCalls/apiCalls.js
+++ apiCalls/apiCalls.js
@@ -2,16 +2,28 @@ export const processAge = async (name) => {
   const url = `https://api.agify.io?name=${name}`
   const response = await fetch(url);
   if (!response.ok) {
-    throw new Error('There was an error processing the age')
+    throw new Error('There was an error processing your age')
   }
   const age = await response.json()
   return age
 }
 
 export const processGender = async (name) => {
-
+  const url = `https://api.genderize.io?name=${name}`
+  const response = await fetch(url)
+  if (!response.ok) {
+    throw new Error('There was an error processing your gender')
+  }
+  const gender = await response.json()
+  return gender
 }
 
-export const getFortune = () => {
-
+export const getAdvice = () => {
+  const url = 'https://api.adviceslip.com/advice'
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error('There was an error getting your advice')
+  }
+  const advice = await response.json()
+  return advice
 }
