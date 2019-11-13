bb94f4c474e81340d58a77ad3f73653758754ce8
Alyssa Lundgren <lundgrea@gmail.com>
2019-11-13 07:57:01 -0700

    Integrate age fetch call

--- apiCalls/apiCalls.js
+++ apiCalls/apiCalls.js
@@ -1,11 +1,17 @@
-export const processAge = () => {
-
+export const processAge = async (name) => {
+  const url = `https://api.agify.io?name=${name}`
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error('There was an error processing the age')
+  }
+  const age = await response.json()
+  return age
 }
 
-export const processGender = () => {
+export const processGender = async (name) => {
 
 }
 
 export const getFortune = () => {
-  
+
 }
