25ef27fd2cd555472589e9558b1ee189cdb275f2
Alyssa Lundgren <lundgrea@gmail.com>
2019-12-05 11:53:58 -0700

    Add processGender async testing

--- src/apiCalls/apiCalls.test.js
+++ src/apiCalls/apiCalls.test.js
@@ -39,3 +39,42 @@ import { processAge, processGender, getAdvice } from '../apiCalls/apiCalls';
       expect(processAge()).rejects.toEqual(Error('There was an error processing your age'));
     });
   });
+
+
+  describe('processGender', () => {
+    let mockResponse;
+    beforeEach(() => {
+      mockResponse = {}
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(mockResponse)
+        });
+      });
+    });
+  
+    it("should call fetch with the correct url", () => {
+      processGender('Alyssa');
+      expect(window.fetch).toHaveBeenCalledWith('https://api.genderize.io?name=Alyssa');
+    });
+  
+    it("should return a successful response (HAPPY)", () => {
+      expect(processGender()).resolves.toEqual(mockResponse);
+    });
+  
+    it("should return an error (SAD)", () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: false
+        });
+      });
+      expect(processGender()).rejects.toEqual(Error('There was an error processing your age'));
+    });
+  
+    it("should return an error if the promise rejects (SAD)", () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.reject(Error('There was an error processing your age'));
+      });
+      expect(processGender()).rejects.toEqual(Error('There was an error processing your age'));
+    });
+  });
